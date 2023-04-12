445a1fe45dd5948ad0c1043b3e0b9e7d43d61a22
Arturo González <arturo@7th.dev>
2023-04-12 08:32:36 -0600

    Add post

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from subscription import Subscription
+import json
 
 app = Flask(__name__)
 
@@ -13,6 +15,15 @@ def getData():
     return jsonify({"suscriptions": suscriptions})
 
 
+@app.route('/register', methods=['POST'])
+def create_subscription():
+    data = json.loads(request.data)
+    name = data['name']
+    amount = data['amount']
+    
+    subscription = Subscription(name, amount)
+    print(subscription)
+    return {'success': True}
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
